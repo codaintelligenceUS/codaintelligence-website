1928fdab70c0a083c7019df57dbcfe6fcc76ec7f
Bogdan Calapod <bogdan.calapod@codaintelligence.com>
2023-07-06 21:20:44 +0300

    feat: Add large style for Demo button

--- src/atoms/DemoButton/DemoButton.tsx
+++ src/atoms/DemoButton/DemoButton.tsx
@@ -2,6 +2,10 @@ import styles from "./DemoButton.module.css";
 /**
  * This is a special button for the Demo modal
  */
-export function DemoButton() {
-  return <button className={styles.button}>Get a Demo</button>;
+export function DemoButton(props: { isLarge?: boolean }) {
+  return (
+    <button className={`${styles.button} ${props.isLarge ? styles.large : ""}`}>
+      Get a Demo
+    </button>
+  );
 }

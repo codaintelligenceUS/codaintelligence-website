9218918e32e9f0b97175bcf0333c0d5680629737
Bogdan Calapod <bogdan.calapod@codaintelligence.com>
2023-07-06 22:11:23 +0300

    feat: Add Title component

---
+++ src/atoms/Title/Title.tsx
@@ -0,0 +1,25 @@
+import styles from "./Title.module.css";
+
+type TitleProps = {
+  /** Main title text */
+  title?: string;
+  /** Subtittle text */
+  subtitle?: string;
+  /** Category text */
+  category?: string;
+  /** Whether text should be left aligned */
+  isLeftAligned?: boolean;
+};
+
+export function Title(props: TitleProps) {
+  return (
+    <div
+      className={`${styles.container} ${props.isLeftAligned ? styles.leftAligned : ""
+        }`}
+    >
+      {props.category && <h3>{props.category}</h3>}
+      {props.title && <h1>{props.title}</h1>}
+      {props.subtitle && <h2>{props.subtitle}</h2>}
+    </div>
+  );
+}

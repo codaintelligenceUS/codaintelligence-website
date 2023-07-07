876bd8aa1bfbc09283e2791758912d065dd414e7
Bogdan Calapod <bogdan.calapod@codaintelligence.com>
2023-07-07 22:20:30 +0300

    feat: Add fixedHeight option to cards title

--- src/sections/CardsTitle/CardsTitle.tsx
+++ src/sections/CardsTitle/CardsTitle.tsx
@@ -5,11 +5,17 @@ import { Card, CardProps } from "@/atoms/Card/Card";
 type CardsTitleProps = {
   title: TitleProps;
   cards: CardProps[];
+  /** Whether to ensure same height for all cards */
+  fixedHeight?: boolean;
 };
 
 export function CardsTitle(props: CardsTitleProps) {
   return (
-    <section className={styles.container}>
+    <section
+      className={`${styles.container} ${
+        props.fixedHeight ? styles.fixedHeight : ""
+      }`}
+    >
       <Title {...props.title} />
 
       <div className={styles.cards}>

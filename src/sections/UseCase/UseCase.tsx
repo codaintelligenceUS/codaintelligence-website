90eb14693634ade71b8716950f75a35b780d06c4
Bogdan Calapod <bogdan.calapod@codaintelligence.com>
2023-07-07 16:17:00 +0300

    task: Extract Report module as common

--- src/sections/UseCase/UseCase.tsx
+++ src/sections/UseCase/UseCase.tsx
@@ -1,8 +1,8 @@
+import { Report } from "@/atoms/Report/Report";
 import { Footer } from "../Footer/Footer";
 import { Header } from "../Header/Header";
 import { Feedback } from "./components/Feedback/Feedback";
 import { HowItWorks } from "./components/HowItWorks/HowItWorks";
-import { Report } from "./components/Report/Report";
 import { Why } from "./components/Why/Why";
 
 export function UseCase() {

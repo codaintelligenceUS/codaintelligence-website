115645046b3c908dde0cac037f0c1a769648f99e
Bogdan Calapod <bogdan.calapod@codaintelligence.com>
2023-07-07 23:03:47 +0300

    task: Add isDarkBlue param to feedback

--- src/sections/Feedback/Feedback.tsx
+++ src/sections/Feedback/Feedback.tsx
@@ -2,11 +2,15 @@ import { Testimonial } from "@/atoms";
 import styles from "./Feedback.module.css";
 import { TestimonialProps } from "@/atoms/Testimonial/Testimonial";
 
-export type FeedbackProps = TestimonialProps;
+export type FeedbackProps = TestimonialProps & { isDarkBlue?: boolean };
 
 export function Feedback(props: FeedbackProps) {
   return (
-    <section className={styles.container}>
+    <section
+      className={`${styles.container} ${
+        props.isDarkBlue ? styles.darkBlue : ""
+      }`}
+    >
       <Testimonial {...props} />
     </section>
   );

c5ec5067d17d35c5288f93f8bf5604a755579367
Bogdan Calapod <bogdan.calapod@codaintelligence.com>
2023-07-07 22:30:44 +0300

    feat: Add customizability to report

--- src/sections/Report/Report.tsx
+++ src/sections/Report/Report.tsx
@@ -3,17 +3,28 @@ import Icon from "./Icon.png";
 import { Title } from "@/atoms/Title/Title";
 import { Button } from "@/atoms";
 
-export function Report() {
+export type ReportProps = {
+  title?: string;
+  subtitle?: string;
+  downloadUrl: string;
+};
+
+export function Report(props: ReportProps) {
   return (
     <section className={styles.container}>
       <img src={Icon} alt="Report icon" />
       <Title
         isLeftAligned
         category="report"
-        title="Take a look on a sample report"
-        subtitle="Footprint brings unparalleled automation into reporting"
+        title={props.title || "Take a look on a sample report"}
+        subtitle={
+          props.subtitle ||
+          "Footprint brings unparalleled automation into reporting"
+        }
       >
-        <Button>Download the report</Button>
+        <a href={props.downloadUrl} target="_blank" rel="noreferrer">
+          <Button>Download the report</Button>
+        </a>
       </Title>
     </section>
   );

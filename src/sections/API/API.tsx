98204526f511332b8050dafe52bc2c02de43867b
Bogdan Calapod <bogdan.calapod@codaintelligence.com>
2023-07-10 01:00:11 +0300

    fix: Further responsive fixes

--- src/sections/API/API.tsx
+++ src/sections/API/API.tsx
@@ -1,6 +1,9 @@
 import { Title } from "@/atoms/Title/Title";
 import styles from "./API.module.css";
 
+import { Button } from "@/atoms";
+import { PAGES } from "@/variables";
+import { Link } from "wouter";
 import Lock from "./Lock.png";
 
 export function API() {
@@ -11,7 +14,11 @@ export function API() {
         isLeftAligned
         title="Already struggling with a complex stack and operational fatigue ?"
         subtitle="Footprint provides an extensive REST API and SDKs for the most popular languages, so the possibilities are endless to integrate our data in your existing processes!"
-      />
+      >
+        <Link to={PAGES.technologyPartners}>
+          <Button>View available integrations</Button>
+        </Link>
+      </Title>
       <img src={Lock} alt="lock" />
     </section>
   );

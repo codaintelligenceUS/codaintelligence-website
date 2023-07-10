252a6df57ec510e63570a23be337837b14406c5c
Bogdan Calapod <bogdan.calapod@codaintelligence.com>
2023-07-10 14:26:22 +0300

    fix: Fix text

--- src/routes/Company.tsx
+++ src/routes/Company.tsx
@@ -7,7 +7,7 @@ export function Company() {
         type="title"
         category="About CODA"
         title="Taking an entirely new approach to security"
-        subtitle="Our vision is to make cyber security available for everyone and embed it into our society's DNA as physical security should be. To achieve our vision, CODA's cyber security products must be Accessible, Affordable and Actionable."
+        subtitle="Our vision is to make cyber security available for everyone and embed it into our society's DNA as it should. To achieve our vision, CODA's cyber security products must be Accessible, Affordable and Actionable."
       />
       <Mission />
       <AsSeenOn />

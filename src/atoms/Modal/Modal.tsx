b76afb286d553a4d5198cd317b7f014779f648d7
Bogdan Calapod <bogdan.calapod@codaintelligence.com>
2023-07-11 18:46:53 +0300

    feat: Update modal interaction
    Added X close button and made it more friendly to interact

--- src/atoms/Modal/Modal.tsx
+++ src/atoms/Modal/Modal.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useRef } from "react";
+import { MouseEventHandler, useEffect } from "react";
 import styles from "./Modal.module.css";
 import { XCircle } from "lucide-react";
 type ModalProps = {
@@ -7,29 +7,38 @@ type ModalProps = {
   children: React.ReactNode;
 };
 export function Modal(props: ModalProps) {
-  const ref = useRef<HTMLDialogElement>(null);
+  const { onDismiss: propsOnDismiss } = props;
 
-  const onDismiss = () => {
-    ref.current?.close();
+  useEffect(() => {
+    const dismissOnEsc = (e: KeyboardEvent) => {
+      if (e.key === "Escape") {
+        propsOnDismiss();
+      }
+    };
+    document.addEventListener("keydown", dismissOnEsc);
+
+    return () => {
+      document.removeEventListener("keydown", dismissOnEsc);
+    };
+  }, [propsOnDismiss]);
+
+  if (!props.isOpen) {
+    return false;
+  }
+
+  const onClick: MouseEventHandler<HTMLDivElement> = (e) => {
+    e.stopPropagation();
     props.onDismiss();
   };
 
-  useEffect(() => {
-    if (props.isOpen) {
-      ref.current?.showModal();
-    } else {
-      ref.current?.close();
-    }
-  }, [props.isOpen]);
-
   return (
-    <dialog ref={ref} className={styles.modal} onCancel={onDismiss}>
-      <div className={styles.container}>
-        <div className={styles.closeButton} onClick={onDismiss}>
+    <div className={styles.modal} onClick={onClick}>
+      <div className={styles.container} onClick={(e) => e.stopPropagation()}>
+        <div className={styles.closeButton} onClick={props.onDismiss}>
           <XCircle />
         </div>
         {props.children}
       </div>
-    </dialog>
+    </div>
   );
 }

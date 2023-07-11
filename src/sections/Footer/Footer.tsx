93e1194977fa5caae3fe6db3ab4ab43c27a9321b
Bogdan Calapod <bogdan.calapod@codaintelligence.com>
2023-07-11 18:25:30 +0300

    feat: Add book demo section to all pages

--- src/sections/Footer/Footer.tsx
+++ src/sections/Footer/Footer.tsx
@@ -3,60 +3,66 @@ import { PAGES } from "@/variables";
 import { Mail, Phone, Pin } from "lucide-react";
 import { Link } from "wouter";
 import styles from "./Footer.module.css";
+import { BookDemo } from "..";
 
 export function Footer() {
   return (
-    <section className={styles.container}>
-      <Logo />
-      <LinkGroup
-        title="Company"
-        links={[{ title: "About us", href: PAGES.company }]}
-      />
-      <LinkGroup
-        title="Platform"
-        links={[
-          {
-            title: "Full Attack Surface Visibility",
-            href: PAGES.fullAttackSurface,
-          },
-          {
-            title: "Contextual Prioritization",
-            href: PAGES.contextualPrioritization,
-          },
-          {
-            title: "Effective Remediation",
-            href: PAGES.effectiveRemediation,
-          },
-        ]}
-      />
-      <LinkGroup
-        title="Partners"
-        links={[
-          { title: "Technology Partners", href: PAGES.technologyPartners },
-          { title: "Managed Service Providers", href: PAGES.mspPartners },
-        ]}
-      />
-      <LinkGroup
-        title="Get in touch"
-        links={[
-          {
-            title: "127 Main St, STE 4, Nashua, NH 03060",
-            href: "#",
-            icon: <Pin />,
-          },
-          {
-            title: "sales@codaintelligence.com",
-            href: "mailto:sales@codaintelligence.com",
-            icon: <Mail />,
-          },
-          {
-            title: "+1 (617) 963-0147",
-            href: "callto:+1 (617) 963-0147",
-            icon: <Phone />,
-          },
-        ]}
-      />
-    </section>
+    <>
+      {" "}
+      <BookDemo />
+      <section className={styles.container}>
+        <Logo />
+        <LinkGroup
+          title="Company"
+          links={[{ title: "Resources", href: PAGES.resources }]}
+          links={[{ title: "About us", href: PAGES.company }]}
+        />
+        <LinkGroup
+          title="Platform"
+          links={[
+            {
+              title: "Full Attack Surface Visibility",
+              href: PAGES.fullAttackSurface,
+            },
+            {
+              title: "Contextual Prioritization",
+              href: PAGES.contextualPrioritization,
+            },
+            {
+              title: "Effective Remediation",
+              href: PAGES.effectiveRemediation,
+            },
+          ]}
+        />
+        <LinkGroup
+          title="Partners"
+          links={[
+            { title: "Technology Partners", href: PAGES.technologyPartners },
+            { title: "Service Providers", href: PAGES.mspPartners },
+          ]}
+        />
+        <LinkGroup
+          title="Get in touch"
+          links={[
+            {
+              title: "127 Main St, STE 4, Nashua, NH 03060",
+              href: "#",
+              icon: <Pin />,
+            },
+            {
+              title: "sales@codaintelligence.com",
+              href: "mailto:sales@codaintelligence.com",
+              icon: <Mail />,
+            },
+            {
+              title: "+1 (617) 963-0147",
+              href: "callto:+1 (617) 963-0147",
+              icon: <Phone />,
+            },
+          ]}
+        />
+      </section>
+    </>
   );
 }
 

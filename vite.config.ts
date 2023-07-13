d63254d77db55e324f2b465eff1c9a3f13ff5bc1
Bogdan Calapod <bogdan.calapod@codaintelligence.com>
2023-07-13 15:03:20 +0300

    feat: Add image optimization steps

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,16 @@
 import path from "path";
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
+import { ViteImageOptimizer } from "vite-plugin-image-optimizer";
+import ViteWebp from "vite-plugin-webp-generator";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    ViteImageOptimizer(),
+    ViteWebp({ extensions: ["png", "jpg"] }),
+  ],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),

f3a9def9ddedb4189dc4d32d6461f5187098280b
Bogdan Calapod <bogdan.calapod@codaintelligence.com>
2023-08-08 15:07:26 +0200

    fix: Allow non-trailing slash URLs

--- src/variables.ts
+++ src/variables.ts
@@ -1,13 +1,13 @@
 // Common variables used throughout the site
 export const PAGES = {
   home: "",
-  company: "company/",
-  technologyPartners: "partners/tech/",
-  mspPartners: "partners/msp/",
-  resources: "resources/",
-  contextualPrioritization: "contextualPrioritization/",
-  effectiveRemediation: "effectiveRemediation/",
-  fullAttackSurface: "fullAttackSurface/",
-  partnerEnablement: "partnerEnablement/",
-  terms: "terms/",
+  company: "company",
+  technologyPartners: "partners/tech",
+  mspPartners: "partners/msp",
+  resources: "resources",
+  contextualPrioritization: "contextualPrioritization",
+  effectiveRemediation: "effectiveRemediation",
+  fullAttackSurface: "fullAttackSurface",
+  partnerEnablement: "partnerEnablement",
+  terms: "terms",
 };
